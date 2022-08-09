ab53d1b81a396af9489a973f0e535fad6496d34d
baraaj <jeridibaraa@gmail.com>
2022-08-09 20:01:36 +0200

    f4 commit

---
+++ src/components/featuredInfo/FeaturedInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './featuredInfo.css';
+const FeaturedInfo = () => {
+    return (
+        <div className='featured'>
+             <div className='featuredItem'>
+            
+            </div>
+        </div>
+    );
+}
+
+export default FeaturedInfo;
